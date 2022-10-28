103f71206a300fab459b023655e66e7076cf3694
tiger <thisisstreamliner@gmail.com>
2022-10-28 23:56:23 +0300

    Make Our Obstacle Moving (Mathf.Sin for Oscillation)

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Oscillator : MonoBehaviour
 {
     [SerializeField] Vector3 movementVector;
-    [SerializeField][Range(0, 1)] float movementFactor;
+    [SerializeField] float period = 2f;
     private Vector3 _startingPosition;
     // Start is called before the first frame update
     void Start()
@@ -16,6 +16,10 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float cycles = Time.time / period; // continually growing over time
+        const float tau = Mathf.PI * 2; // constant value of 6.283
+        float rawSinWave = Mathf.Sin(cycles * tau); // going from -1 to 1
+        float movementFactor = (rawSinWave + 1f) / 2f; //recalculated to go from 0 to 1 so its cleaner
         Vector3 offset = movementVector * movementFactor;
         transform.position = _startingPosition + offset;
     }
